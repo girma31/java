e12edcc6c5861a024220276c2d5c3e5adcbdcef4
girmahcew31 <girmachewabebe@gmail.com>
2021-04-20 13:08:22 +0300

    welcome message is added

--- Bank.java
+++ Bank.java
@@ -2,6 +2,8 @@
 class Bank{
 
    public static void main(String[] args) {
+     System.out.println("-----------------------------------------------------------------");
+     System.out.println("------------------------Welcome-----------------------------------");
      Account account = new Account();
      account.setAccount_balance(10000);
      account.credit(500);
