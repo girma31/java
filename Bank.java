0743f1dc543a4a1108333cded36ff2841ebda3a4
girmahcew31 <girmachewabebe@gmail.com>
2021-04-20 13:19:10 +0300

    functionalities

--- Bank.java
+++ Bank.java
@@ -4,6 +4,16 @@ class Bank{
    public static void main(String[] args) {
      System.out.println("-----------------------------------------------------------------");
      System.out.println("------------------------Welcome-----------------------------------");
+     /*
+              what do want 
+              deposit 
+              withdraw
+              transfer
+              check balance
+              pay bill
+              
+
+     */
      Account account = new Account();
      account.setAccount_balance(10000);
      account.credit(500);
